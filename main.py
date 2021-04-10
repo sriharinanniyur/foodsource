1d75c5e594b03260be980b8e6f49e684ababea74
Aadit Trived <aadit.trivedi03@gmail.com>
2021-04-10 11:41:37 -0700

    connected order form to local db

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import *
+import csv
 app = Flask(__name__)
 
 
@@ -6,9 +7,26 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/order')
+@app.route('/order', methods=['GET', 'POST'])
 def order():
+    if (request.method == "POST"):
+        fname = request.form['fname']
+        income = request.form["income"]
+        faddress = request.form["first-address"]
+        saddress = request.form["second-address"]
+        full_address = faddress + ", " + saddress
+        food = request.form["food"]
+        data = [fname, income, full_address, food]
+        print(data)
+
+        with open('db/db.csv', 'a') as db_file:
+            employee_writer = csv.writer(db_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            employee_writer.writerow(data)
+        
+
     return render_template("order.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
